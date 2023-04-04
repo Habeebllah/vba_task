5601a440160a6a8a0b42e366858082bd4182a347
Habeebllah <lasisihabeeb67@gmail.com>
2023-04-04 21:43:02 +0100

    Header Completed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Header from "@/components/Header/Header";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,9 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello World!!!</h1>
+      <main className="md:flex flex-col w-full fixed top-0">
+        <Header />
       </main>
     </>
-  )
+  );
 }
